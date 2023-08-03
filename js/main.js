f2bc1756207a80374a11530e5fa228eac400ee9b
masuden0000 <hudarasyad456@gmail.com>
2023-08-03 10:51:37 +0700

    fix(body): fix bug responsive at button and home-footer

--- js/main.js
+++ js/main.js
@@ -22,45 +22,22 @@ function validateForm() {
     var emailInput = document.getElementById("email");
     var interestInput = document.getElementById("interest");
 
-    // Validasi input nama menggunakan pattern regex
     var namePattern = /^[A-Za-z ]+$/;
     if (!namePattern.test(nameInput.value)) {
-        nameInput.classList.add("error"); // Tambahkan class "error" untuk merubah border menjadi merah
         showErrorMessage(nameInput, "Please enter your name using alphabets only.");
         return false;
-    } else {
-        nameInput.classList.remove("error"); // Hilangkan class "error" jika input valid
     }
 
-    // Validasi email menggunakan input type="email"
     if (!emailInput.checkValidity()) {
-        emailInput.classList.add("error"); // Tambahkan class "error" untuk merubah border menjadi merah
         showErrorMessage(emailInput, "Please enter a valid email address.");
         return false;
-    } else {
-        emailInput.classList.remove("error"); // Hilangkan class "error" jika input valid
     }
 
-    // Validasi dropdown box
     if (interestInput.value === "") {
-        interestInput.classList.add("error"); // Tambahkan class "error" untuk merubah border menjadi merah
         showErrorMessage(interestInput, "Please select an option from the dropdown.");
         return false;
-    } else {
-        interestInput.classList.remove("error"); // Hilangkan class "error" jika input valid
     }
 
-    // Formulir valid, dapatkan data dari formulir dan kirim ke server
-    // Contoh: var formData = new FormData(document.querySelector('form'));
-    // Kirimkan formData ke server menggunakan AJAX atau submit form biasa
     return true;
 }
 
-function showErrorMessage(inputElement, message) {
-    var errorMessageElement = document.createElement("div");
-    errorMessageElement.className = "error-message";
-    errorMessageElement.textContent = message;
-
-    var container = inputElement.parentElement;
-    container.appendChild(errorMessageElement);
-}
